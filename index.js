8d27239b70af5343ff37e2d088cfac7c0a892f15
Ian Clark <IanClark2021@u.northwestern.edu>
2021-10-23 14:48:03 -0500

    Added file over write functionality using fs.existsSync

--- index.js
+++ index.js
@@ -13,9 +13,37 @@ const Employee = require('./lib/Employee.js');
 const menuChoices = ['Add an Engineer', 'Add an Intern', 'Or finish building the team'];
 
 let results;
+
+// Start HTML 
+const beginFile = `<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]>      <html class="no-js"> <!--<![endif]-->
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Team Rolodex</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="./styles.css">
+        
+    </head>
+    <body>
+        <header></header>
+        <main>`;
 // Final Team Members Array 
 let finalTeam = [];
 
+let overwriteFile = [
+    {
+        type: 'confirm', 
+        name: 'overwrite', 
+        message: 'Would you like to overwrite the previous team page and start again?',
+        default: false
+    },
+]
 
 const githubPlease = {
     type: 'input',
@@ -70,6 +98,20 @@ let employeeQuestions = [
 
 
 // After the init function, 
+const fireQuestions = () => {
+    inquirer.prompt(employeeQuestions)
+        // Grab & Evaluate the Answer Data  
+    .then((firstAnswers) => {
+        employeeQuestions.shift()
+            // The first promise, returns data specific to the manager role. 
+        let teamMember = new Manager(firstAnswers.employeeName, firstAnswers.employeeID, firstAnswers.employeeEmail, firstAnswers.mgmtOffice);
+        // After constructing the manager, push it to our team array. 
+        finalTeam.push(teamMember);
+
+        mutateQuestions(firstAnswers);
+    })
+}
+
 const mutateQuestions = (data) => {
    // If the user selects an option other "finish build", 
     if (data.menuOption === 'Add an Engineer') { 
@@ -93,66 +135,59 @@ const mutateQuestions = (data) => {
             mutateQuestions(data);
         })
     } else { // If we're not adding a new team member, exit the function with the built-up array
-       return startPage(finalTeam);
+        startPage(finalTeam); // deleted return before function
     }
-     
 }
 
 const startPage = (arrayOfMembers) => {
-        return new Promise(function(resolve, reject) {
-       
-            for (let i = 0; i < arrayOfMembers.length; i++) {
-                
-                let section = arrayOfMembers[i].paintToPage();
-                console.log(section);
-                fs.appendFile('./dist/index.html', section, function (err) {
-                    if (err) { return reject(err); };
-                    return resolve();
-                })
-            }
-            
-            finalizePage(); 
-         })
-         
-    
-}
-
-const finalizePage = () => {
     const endPage = `        
+    </main>
     <!--[if lt IE 7]>
         <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="#">upgrade your browser</a> to improve your experience.</p>
     <![endif]-->
     <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
     <script src="../src/generatePage.js" async defer></script>
-</body>
-</html>`;
-    return new Promise(function(resolve, reject) {
-        fs.appendFile('./dist/index.html', endPage, function(err) {
-            if (err) { return reject(err) }
-            return resolve();
+    </body>
+    </html>`;
+    return new Promise(function(resolve, reject) {       
+        for (let i = 0; i < arrayOfMembers.length; i++) {
+            
+            let section = arrayOfMembers[i].paintToPage();
+            console.log(section);
+            fs.appendFile('./dist/index.html', section, function (err) {
+                if (err) { return reject(err); };
+                return resolve();
+            })
+        }    
+        return new Promise(function(resolve, reject) {
+            fs.appendFile('./dist/index.html', endPage, function(err) {
+                if (err) { return reject(err) }
+                return resolve();
+            })
         })
+        
     })
+    
 }
-
 
 // Define Initialization function 
 const init = () => {
-    inquirer.prompt(employeeQuestions)
-
-       // Grab & Evaluate the Answer Data  
-    .then((firstAnswers) => {
-        employeeQuestions.shift()
-
-         // The first promise, returns data specific to the manager role. 
-        let teamMember = new Manager(firstAnswers.employeeName, firstAnswers.employeeID, firstAnswers.employeeEmail, firstAnswers.mgmtOffice);
-        // After constructing the manager, push it to our team array. 
-        finalTeam.push(teamMember);
-
-
-        mutateQuestions(firstAnswers);
-        
-    })
+    if (fs.existsSync('./dist/index.html')) {
+        inquirer.prompt(overwriteFile)
+        .then((overwriteResponse) => {
+            if (!overwriteResponse.response) {
+                fs.writeFile('./dist/index.html', beginFile, (err) =>
+                    err ? console.error(err) : console.log('file created')
+                )
+                fireQuestions();
+            } else {
+                console.log('To create a new page, please move your index.html to a new folder');
+            }
+        })
+    }
 }
+
+
 
 // Call Initialization function 
 init();
